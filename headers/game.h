70df23e6cbfb8edd92dc8f543a95ea20aee18b52
Spyro1 <redstone.mc01@gmail.com>
2023-11-27 19:13:46 +0100

    Végleges Beadás

--- headers/game.h
+++ headers/game.h
@@ -39,21 +39,21 @@ bool game_Init(Player *player, char **levelList);
  * @param movesListHead Move** A játékos lépéseit eltároló láncolt lista. (Cím szerint)
  * @return bool Igaz, ha újraindítja a játékos a szinten; Különben hamis
  */
-static bool game_KeyPress(CellType **map, Size mapSize, int *numOfMoves, Player *player, Point *playerPosition, Point *boxPositions, Move **movesListHead);
+//static bool game_KeyPress(CellType **map, Size mapSize, int *numOfMoves, Player *player, Point *playerPosition, Point *boxPositions, Move **movesListHead);
 /**
  * Ez a függvény indítja el és futtatja ciklikusan a játékot.
  * @param player Player* Az aktuális játékos adatait tartalmazza. (Cím szerint)
  * @param levelName char[] A betöltendő pálya fájlneve.
  * @return bool Igaz, ha a játékos teljesítette a szintet; Hamis, ha a játékos kilépett a játékból.
  */
-static bool game_StartGame(Player *player, char levelName[]);
+//static bool game_StartGame(Player *player, char levelName[]);
 /**
  * Ellenőrzi, hogy a játékos teljesítette-e a szintet, vagyis, hogy minden doboz a helyére került-e.
  * @param map CellType** A pályát leíró 2D-s dinamikus mátrix.
  * @param mapSize Size A pálya szélességét és magasságát leíró struktúra.
  * @return bool Igaz, ha minden doboz a helyére került; Hamis, ha van egy doboz is, ami nincs a helyén.
  */
-static bool game_CheckWin(CellType **map, Size mapSize);
+//static bool game_CheckWin(CellType **map, Size mapSize);
  /**
  * A játékos elmozdulását tesztelő függvény a direction irányba.
  * Ha lehetséges a lépés, vagy ha doboz van a lépés irányában akkor a dobozt eltolja, és igazat ad vissza; Hamis, ha nem lehetséges a lépés.
@@ -64,7 +64,7 @@ static bool game_CheckWin(CellType **map, Size mapSize);
  * @param movesListHead Move** A játékos lépéseit eltároló láncolt lista. (Cím szerint)
  * @return bool Igaz, ha el tud mozdulni a játékos az adott irányba; Hamis, ha nem lehetséges a lépés.
  */
-static bool game_MovePlayer(CellType ***map, Point *currentPosition, Point **boxPositions, Point direction, Move **movesListHead);
+//static bool game_MovePlayer(CellType ***map, Point *currentPosition, Point **boxPositions, Point direction, Move **movesListHead);
 /**
  * Visszavonja a játékos előző lépést. Egészen addig fut le sikeresen,
  * amíg a moveList-ben volt elem, azaz meglépett lépés.
@@ -74,7 +74,7 @@ static bool game_MovePlayer(CellType ***map, Point *currentPosition, Point **box
  * @param moveListHead Move** A játékos lépéseit eltároló láncolt lista (Cím szerint)
  * @return bool Igaz, ha sikeresen visszavonta a lépést; Hamis, ha nincs több visszavonható lépés
  */
-static bool game_UndoMove(CellType ***map, Point *currentPosition, Point **boxPositions, Move **moveListHead);
+//static bool game_UndoMove(CellType ***map, Point *currentPosition, Point **boxPositions, Move **moveListHead);
 
 // == BEOLVASÁS ==
 /**
@@ -86,13 +86,13 @@ static bool game_UndoMove(CellType ***map, Point *currentPosition, Point **boxPo
  * @param boxPositions Point** A dobozok koordinátáinak dinamikus tömbje (Cím szerint, kimenet)
  * @param boxCount int* A dobozok koordinátáit tároló dinamikus tömb elemszáma, dobozok száma a pályán (Cím szerint, kimenet)
  */
-static void game_ReadXSBFile(char filename[], CellType ***map, Size *mapSize, Point *playerPosition, Point **boxPositions, int *boxCount);
+//static void game_ReadXSBFile(char filename[], CellType ***map, Size *mapSize, Point *playerPosition, Point **boxPositions, int *boxCount);
 /**
  * A beolvasott fájl egy karakterét értelmezi és átalakítja cellType értékké
  * @param character char char A beolvasott fájl egy karaktere
  * @return CellType A kapott karakter értelmezett CellType értékekké alakított értéke
  */
-static CellType game_ConvertInputCharToCellType(char character);
+//static CellType game_ConvertInputCharToCellType(char character);
 
 // == KIIRATÁS ==
 /**
@@ -100,20 +100,20 @@ static CellType game_ConvertInputCharToCellType(char character);
  * @param map CellType** A pályát leíró 2D-s dinamikus mátrix
  * @param mapSize Size A pálya szélességét és magasságát leíró struktúra
  */
-static void game_PrintStyledMap(CellType **map, Size mapSize);
+//static void game_PrintStyledMap(CellType **map, Size mapSize);
 /**
  * Egy kapott koordinátán lévő mezőt írja ki a képernyőre színesen és a megfelelő definiált karaterrel
  * @param map CellType** A pályát leíró 2D-s dinamikus mátrix
  * @param pos Point A kiirandó karakter koordinátája
  */
-static void game_PrintPosition(CellType **map, Point pos);
+//static void game_PrintPosition(CellType **map, Point pos);
 /**
  * Kiírja a képernyőre a játéktér mellé az aktuális szintet és a lépések számát, vagy a tutorial pályánál a bevezető instrukciókat
  * @param mapSize Size A pálya szélességét és magasságát leíró struktúra
  * @param numOfSteps int A szinten megtett lépések száma
  * @param level int Az aktuális szint száma
  */
-static void game_PrintStatsAndNav(Size mapSize, int numOfSteps, int level);
+//static void game_PrintStatsAndNav(Size mapSize, int numOfSteps, int level);
 
 // == MEMÓRIAFOGLALÁS ÉS FELSZABADÍTÁS ==
 /**
@@ -121,22 +121,22 @@ static void game_PrintStatsAndNav(Size mapSize, int numOfSteps, int level);
  * @param map CellType*** A pályát leíró 2D-s dinamikus mátrix (Cím szerint)
  * @param mapSize Size* A pálya szélességét és magasságát leíró struktúra
  */
-static void game_AllocateMemoryToMap(CellType ***map, Size *mapSize);
+//static void game_AllocateMemoryToMap(CellType ***map, Size *mapSize);
 /**
  * Memóriát foglal egy 1D-s dinamikus tömbnek
  * @param newArray Point** Az új 1D-s dinamikus tömb címe (Cím szerint)
  * @param lenght int A létrehozandó dinamikus tömb hossza, elemszáma
  */
-static void game_AllocateDynamicArray(Point **newArray, int lenght);
+//static void game_AllocateDynamicArray(Point **newArray, int lenght);
 /**
  * Felszabadítja a pályát tároló 2D-s dinamikus mátrix lefoglat memóriáját, ha volt lefoglalva
  * @param map Celltype*** A pályát leíró 2D-s dinamikus mátrix (Cím szerint)
  */
-static void game_FreeAllocatedMemoryFromMap(CellType ***map);
+//static void game_FreeAllocatedMemoryFromMap(CellType ***map);
 /**
  * Felszabadítja egy 1D-s dinamikus tömbnek lefoglalt memóriáját, ha volt lefoglalva
  * @param dynamicArray Point** 1D-s dinamikus tömb címe (Cím szerint)
  */
-static void game_FreeDynamicArray(Point **dynamicArray);
+//static void game_FreeDynamicArray(Point **dynamicArray);
 
 #endif //GITIGNORE_GAME_H
--- headers/menu.h
+++ headers/menu.h
@@ -25,30 +25,30 @@
 /**
  * Kiirítja a főcímet a képernyő tetejére nagy betűkkel több sorosan.
  */
-static void menu_PrintTitle();
+//static void menu_PrintTitle();
 /**
  * Kiírja a képernyő aljára az aktuális menüpot nevigációs lehetőségeit.
  */
-static void menu_PrintNavControls();
+//static void menu_PrintNavControls();
 
 // = Almenü kiíró függvények =
 /**
  * Kiír a képernyére egy ablakot, amiben megkérdezi a felhasználót, hogy biztos ki akar e lépni, Igen/Nem válaszlehetőségekkel.
  * @param p Point A kiíráshoz legfelső középső pont a képernyőn.
  */
-static void menu_PrintExitWindow(Point p);
+//static void menu_PrintExitWindow(Point p);
 /**
  * Kiírja a képernyőre a főmenü menüpontjait, és kijelöli az aktuálisan kiválasztott menüpontot más színnel.
  * @param p Point A kiíráshoz legfelső középső pont a képernyőn.
  */
-static void menu_PrintMainMenu(Point p);
+//static void menu_PrintMainMenu(Point p);
 /**
  * Kiírja a képernyőre az új játékos felvételéhez szükséges mezőt, és a bemenet után hozzáadja a játéklistához az új játékost.
  * @param playerListHead Player** A játékosok adatait tartalmazó láncolt lista. (Cím szerint)
  * @param numOfPlayers int* A játékosok darabszáma. (Cím szerint)
  * @param p Point A kiíráshoz legfelső középső pont a képernyőn.
  */
-static void menu_PrintNewPlayerSubMenu(Player **playerListHead, int *numOfPlayers, Point p);
+//static void menu_PrintNewPlayerSubMenu(Player **playerListHead, int *numOfPlayers, Point p);
 /**
  * Kiírja a képernyőre a játékoslistát, és kijelöli az aktuálisan kiválasztott játékost más színnel.
  * @param playerListHead Player** A játékosok adatait tartalmazó láncolt lista. (Cím szerint)
@@ -56,24 +56,24 @@ static void menu_PrintNewPlayerSubMenu(Player **playerListHead, int *numOfPlayer
  * @param numOfPlayers int* A játékosok darabszáma. (Cím szerint)
  * @param p Point A kiíráshoz legfelső középső pont a képernyőn.
  */
-static void menu_PrintPlayerSubMenu(Player **playerListHead, Player **currentPlayer,  int *numOfPlayers, Point p );
+//static void menu_PrintPlayerSubMenu(Player **playerListHead, Player **currentPlayer,  int *numOfPlayers, Point p );
 /**
  * Kiírja a képernyőre a dicsőséglistát táblázatosan.
  * @param playerListHead Player** A játékosok adatait tartalmazó láncolt lista. (Cím szerint)
  * @param numOfPlayers int* A játékosok darabszáma. (Cím szerint)
  * @param p Point A kiíráshoz legfelső középső pont a képernyőn.
  */
-static void menu_PrintRankList(Player **playerListHead, int *numOfPlayers, Point p);
+//static void menu_PrintRankList(Player **playerListHead, int *numOfPlayers, Point p);
 /**
  * Kiírja a képernyőre, hogy teljesítette a szinteket a játékos.
  * @param p Point A kiíráshoz legfelső középső pont a képernyőn.
  */
-static void menu_PrintWinGame(Point p);
+//static void menu_PrintWinGame(Point p);
 
 /**
  * Visszaállítja a kezdőértékeket a menüben, ha menüpontváltás volt.
  */
-static void menu_ResetMenuVars();
+//static void menu_ResetMenuVars();
 /**
  * Kiértékeli a felhasználó által lenyomott billentyőt, és megváltoztatja a program állapotát aszerint.
  * @param currentPlayer Player* A kiválasztott játékos struktúrájára mutató pointer, playerListHead elem.
@@ -81,7 +81,7 @@ static void menu_ResetMenuVars();
  * @param numOfPlayers *char[] A játékosok darabszáma. (Cím szerint)
  * @param levelList int A szintek fájlnevét tároló dinamikus string tömb.
  */
-static void menu_KeyPress(Player *currentPlayer, Player **playerListHead, int *numOfPlayers, char *levelList[], int numOfLevels);
+//static void menu_KeyPress(Player *currentPlayer, Player **playerListHead, int *numOfPlayers, char *levelList[], int numOfLevels);
 /**
  * Az aktuális állapotnak megfelelően végrehajtja a szükséges utasításokat, és kiértékeli a bemeneteket.
  * @param playerListHead Player** A játékosok adatait tartalmazó láncolt lista. (Cím szerint)
@@ -90,7 +90,7 @@ static void menu_KeyPress(Player *currentPlayer, Player **playerListHead, int *n
  * @param p Point A kiíráshoz legfelső középső pont a képernyőn.
  * @param linesPrinted int* Az előző menüpontba a képernyőre írt sorok.
  */
-static void menu_EvaluateState(Player **playerListHead, int *numOfPlayers, Player **currentPlayer, Point p, int *linesPrinted);
+//static void menu_EvaluateState(Player **playerListHead, int *numOfPlayers, Player **currentPlayer, Point p, int *linesPrinted);
 
 /**
  * A főmenüt futtató függvény. Egyszer hivandó meg a mainben.
